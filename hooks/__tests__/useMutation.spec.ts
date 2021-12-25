5509e86d25ea95d28b7902f43a8e82192af4c6e2
avrcoelho <andrevrcoelho@hotmail.com>
2021-12-25 13:48:54 -0300

    test: configuration

--- hooks/__tests__/useMutation.spec.ts
+++ hooks/__tests__/useMutation.spec.ts
@@ -3,7 +3,7 @@ import { renderHook, act } from "@testing-library/react-hooks";
 import { useMumation } from "../useMutation";
 
 describe("useMutation hook", () => {
-  it("should be able to return data after success request", async () => {
+  it("should be able to return success on request", async () => {
     const { result, waitForNextUpdate } = renderHook(() =>
       useMumation(() => Promise.resolve(true))
     );
@@ -14,5 +14,18 @@ describe("useMutation hook", () => {
     await waitForNextUpdate();
 
     expect(result.current.isSuccess).toBeTruthy();
+  });
+
+  it("should be able to return error on request", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useMumation(() => Promise.reject(true))
+    );
+
+    act(() => {
+      result.current.mutate();
+    });
+    await waitForNextUpdate();
+
+    expect(result.current.isError).toBeTruthy();
   });
 });
