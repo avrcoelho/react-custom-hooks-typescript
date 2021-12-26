b047b34da1ecd1b08ba1ac35b80cc636cb82b9ea
avrcoelho <andrevrcoelho@hotmail.com>
2021-12-26 11:37:00 -0300

    test: useDebounced value hook

---
+++ hooks/__tests__/useDebouncedValue.spec.ts
@@ -0,0 +1,14 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import { useDebouncedValue } from "../useDebouncedValue";
+
+jest.useFakeTimers();
+
+describe("useDebouncedValue hook", () => {
+  it("should be able to return value", async () => {
+    const { result } = renderHook(() => useDebouncedValue("test"));
+    jest.advanceTimersByTime(500);
+
+    expect(result.current).toBe("test");
+  });
+});
