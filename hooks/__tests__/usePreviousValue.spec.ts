1cf12cff85e0e7eb89fde629c4eaa249b9d4caf0
avrcoelho <andrevrcoelho@hotmail.com>
2021-12-27 21:21:18 -0300

    test: usePrevious hook

---
+++ hooks/__tests__/usePreviousValue.spec.ts
@@ -0,0 +1,11 @@
+import { renderHook } from "@testing-library/react-hooks";
+
+import { usePreviousValue } from "../usePreviousValue";
+
+describe("usePreviousValue hook", () => {
+  it("should be able to change value", async () => {
+    const { result } = renderHook(() => usePreviousValue("test"));
+
+    expect(result.current).toBe("test");
+  });
+});
